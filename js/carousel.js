b8af71b0d9e6b6ef2bcccf4277ce8f850fc4c79c
LAPTOP-G36SIJ74\Usuario <fsperonip@hotmail.com>
2018-03-04 21:19:33 -0300

    Arreglos, galeria nueva

--- js/carousel.js
+++ js/carousel.js
@@ -1,17 +1,20 @@
 jQuery(document).ready(function( $ ) {
 
   // Initiate the wowjs animation library
-  new WOW().init();
+  //new WOW().init();
 
   // Initiate superfish on nav menu
+  /*
   $('.nav-menu').superfish({
     animation: {
       opacity: 'show'
     },
     speed: 400
   });
+  */
 
   // Intro carousel
+  /*
   var introCarousel = $(".carousel");
   var introCarouselIndicators = $(".carousel-indicators");
   introCarousel.find(".carousel-inner").children(".carousel-item").each(function(index) {
@@ -27,5 +30,21 @@ jQuery(document).ready(function( $ ) {
     },
     allowPageScroll:"vertical"
   });
+  */
+
+  // Clients carousel (uses the Owl Carousel library)
+  $(".clients-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: { 0: { items: 2 }, 768: { items: 3 }, 900: { items: 5 }
+    }
+  });
+
+  lightbox.option({
+      "showImageNumberLabel": false,
+    })
+
 
 });
+
