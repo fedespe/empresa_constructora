043825d58d8c592e14817d7b66c5eaef49a629e7
BRUNO\Bruno <brunoediaz18@gmail.com>
2018-03-14 11:39:12 -0300

    Modificación de cuenta de correo a la que llegan los mails.
    Se envían tanto a la cuenta de info como a la de Wiliam.

--- php/envioCorreo.php
+++ php/envioCorreo.php
@@ -43,7 +43,7 @@ $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 //Correo a donde llega el mail. Se pueden agregar varios de la misma forma. El segundo parámetro es opcional, es el nombre que figura en la lista de correos.
 $mail->addAddress('info@wfconstrucciones.com', 'WF Construcciones');
-//$mail->addAddress('deortaw@gmail.com', 'Wiliam');
+$mail->addAddress('deortaw@gmail.com', 'Wiliam');
 //Correo con copia oculta. Se pueden agregar varios de la misma forma.
 $mail->addBCC('bdiaz@bigcheese.com.uy');
 $mail->addBCC('fsperonip@gmail.com');
